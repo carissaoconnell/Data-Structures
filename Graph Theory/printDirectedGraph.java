82a2ac0179a621b7e23e407ac771848250e1a61e
carissaoconnell <148776786+carissaoconnell@users.noreply.github.com>
2024-07-03 15:54:32 -0400

    Update printDirectedGraph.java

--- Graph Theory/printDirectedGraph.java
+++ Graph Theory/printDirectedGraph.java
@@ -2,78 +2,82 @@
 
 import java.util.*;
 
-    public class Graph {
-        int vertex;
-        LinkedList<Integer> adjacencyList[];
-        
-        public Graph(int vertex) {
-            this.vertex = vertex;
-            adjacencyList = new LinkedList[vertex];
-            for (int i = 0; i <vertex ; i++) {
-                adjacencyList[i] = new LinkedList<>();
-            }
-        }
+public class DirectedGraph {
+    int vertex;
+    LinkedList<Integer> adjacencyList[];
 
-        public void addEdge(int source, int destination) {
-            //add edge
-            adjacencyList[source].addFirst(destination);
-            //add back edge (for undirected)
-            adjacencyList[destination].addFirst(source);
+    public DirectedGraph(int vertex) {
+        this.vertex = vertex;
+        adjacencyList = new LinkedList[vertex];
+        for (int i = 0; i < vertex; i++) {
+            adjacencyList[i] = new LinkedList<>();
         }
+    }
 
-        public void printGraph(){
-            for (int i = 0; i <vertex ; i++) {
-                if(adjacencyList[i].size() > 0) {
-                    System.out.print("Vertex " + i + " is connected to: ");
-                        for (int j = 0; j < adjacencyList[i].size(); j++) {
-                            System.out.print(adjacencyList[i].get(j) + " ");
-                        }
-                System.out.println();
-                }
+    public void addEdge(int source, int destination) {
+        // Add edge from source to destination
+        adjacencyList[source].add(destination);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Vertex " + i + " is connected to: ");
+            for (int j = 0; j < adjacencyList[i].size(); j++) {
+                System.out.print(adjacencyList[i].get(j) + " ");
             }
+            System.out.println();
         }
+    }
 
 
     public static void main(String[] args) {
 
-        int numEdges, startNode, destinationNode;
+        int numVertices, numEdges, startNode, destinationNode;
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Enter the number of edges in the graph: ");
-            numEdges = userInput.nextInt();
-		Graph graph = new Graph(numEdges);
+        System.out.println("Enter the number of vertices in the graph: ");
+        numVertices = userInput.nextInt();
+        DirectedGraph graph = new DirectedGraph(numVertices);
 
-        while(numEdges != 0) {
+        System.out.println("Enter the number of edges in the graph: ");
+        numEdges = userInput.nextInt();
+
+        while(numEdges!= 0) {
             numEdges = numEdges - 1;
             System.out.println("Enter the <start node> and its <connected node> : ");
             startNode = userInput.nextInt();
             destinationNode = userInput.nextInt();
             graph.addEdge(startNode,destinationNode);
         }
-            graph.printGraph();
+        graph.printGraph();
     }
 }
-
 
 /*
 
 Example Inputs:
 
-Number of Edges in the graph: 7
-
+Enter the number of vertices in the graph: 
+4
+Enter the number of edges in the graph: 
+7
+Enter the <start node> and its <connected node> : 
 0 1
+Enter the <start node> and its <connected node> : 
 0 4
+Enter the <start node> and its <connected node> : 
 1 2
+Enter the <start node> and its <connected node> : 
 1 3
+Enter the <start node> and its <connected node> : 
 1 4
+Enter the <start node> and its <connected node> : 
 2 3
+Enter the <start node> and its <connected node> : 
 3 4
-
-Expected Output:
-Vertex 0 is connected to: 4 1 
-Vertex 1 is connected to: 4 3 2 0 
-Vertex 2 is connected to: 3 1 
-Vertex 3 is connected to: 4 2 1 
-Vertex 4 is connected to: 3 1 0 
+Vertex 0 is connected to: 1 4 
+Vertex 1 is connected to: 2 3 4 
+Vertex 2 is connected to: 3 
+Vertex 3 is connected to: 4 
 
  */
