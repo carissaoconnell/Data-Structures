8c4e4f5a9b3474c89649c4bff7192fe34b6e2a17
carissaoconnell <148776786+carissaoconnell@users.noreply.github.com>
2024-07-06 16:02:30 -0400

    Add files via upload

---
+++ Graph Theory/Directed Graph - Nodes.cpp
@@ -0,0 +1,86 @@
+// directed graph using nodes
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Graph {
+private:
+    unordered_map<string, vector<string>> verticiesList;
+    vector<string> nodeOrder;
+
+public:
+    void addNode(string node) {
+        verticiesList[node] = {};
+        nodeOrder.push_back(node);
+    }
+
+    void addEdge(string node1, string node2) {
+        verticiesList[node1].push_back(node2);
+    }
+
+    void printGraph() {
+        for (auto& node : nodeOrder) {
+            cout << node << " -> ";
+            for (auto& neighbor : verticiesList[node]) {
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph graph;
+    int numNodes, numEdges;
+
+    cout << "Enter the number of nodes: ";
+    cin >> numNodes;
+
+    for (int i = 0; i < numNodes; i++) {
+        string node;
+        cout << "Enter node " << i + 1 << ": ";
+        cin >> node;
+        graph.addNode(node);
+    }
+
+    cout << "Enter the number of edges: ";
+    cin >> numEdges;
+
+    for (int i = 0; i < numEdges; i++) {
+        string node1, node2;
+        cout << "Enter edge " << i + 1 << " (node1 node2): ";
+        cin >> node1 >> node2;
+        graph.addEdge(node1, node2);
+    }
+
+    cout << "Graph:" << endl;
+    graph.printGraph();
+
+    return 0;
+}
+
+/*
+
+Example
+
+Enter the number of nodes: 5
+Enter node 1: 0
+Enter node 2: 1
+Enter node 3: 2
+Enter node 4: 3
+Enter node 5: 4
+Enter the number of edges: 4
+Enter edge 1 (node1 node2): 0 1
+Enter edge 2 (node1 node2): 1 2
+Enter edge 3 (node1 node2): 2 3
+Enter edge 4 (node1 node2): 3 4
+Graph:
+0 -> 1 
+1 -> 2 
+2 -> 3 
+3 -> 4 
+4 -> 
+*/
